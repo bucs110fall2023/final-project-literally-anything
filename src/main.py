690f259bbb1cb812c3936bf2632f871b7c30afa6
Andrew Zou <azou2@binghamton.edu>
2023-12-01 01:45:19 -0500

    added classes and updated README classes list

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import pygame
+from controller import Controller
+from player import Player
+from obstacles import Obstacles
+
+
+def main():
+    pygame.init()
+    #Create an instance on your controller object
+    #Call your mainloop
+    
+    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
+
+# https://codefather.tech/blog/if-name-main-python/
+if __name__ == '__main__':
+    main()
