482f59f7dd3e27da1fb8d1a421c08fe7e41e5dc6
stevenlin22 <slin22@binghamton.edu>
2023-12-06 16:52:33 -0500

    Obstacles maybe done

--- src/obstacles.py
+++ src/obstacles.py
@@ -4,8 +4,6 @@ import random
 class Obstacles(pygame.sprite.Sprite):
     def __init__(self, x, y, obstacle_type):
         super().__init__()
-        self.x = x
-        self.y = y
         self.obstacle_type = obstacle_type
         self.image = pygame.image.load(f"assets/{obstacle_type}.png")
         self.rect = self.image.get_rect()
@@ -16,15 +14,27 @@ class Obstacles(pygame.sprite.Sprite):
         self.rect.x -= 5
 
 class Asteroid(Obstacles):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y, obstacle_type):
+        super().__init__(x, y, obstacle_type)
         asteroid_heights = [250, 290, 320]
         self.image = pygame.image.load("assets/Asteroid.png")
         self.rect = self.image.get_rect()
         self.rect.centery = asteroid_heights[random.randrange(0,3)]
-        self.y_speed = 0
+        self.x_speed = 0
     def update(self):
-        self.y_speed += 1
-        self.rect.y += self.y_speed
+        self.x_speed += 1
+        self.rect.x += self.x_speed
     def draw(self,screen):
-        screen.blit(self.image, (self.x,self.y))
+        screen.blit(self.image, (self.rect.x,self.rect.y))
+
+class Radar(Obstacles):
+    def __init__(self, x, y, obstacle_type):
+        super().__init__(x, y, obstacle_type)
+        self.image = pygame.image.load("assests/Radar.png")
+        self.react = self.image.get_rect()
+        self.x_speed = 0
+    def update(self):
+        self.x_speed += 1
+        self.rect.x += self.x_speed
+    def draw(self,screen):
+        screen.blit(self.image, (self.rect.x,self.rect.y))
