69e4f5df2a430a7cff380190157b1b3a96699535
stevenlin22 <slin22@binghamton.edu>
2023-12-08 16:29:25 -0500

    centered game over screen

--- src/controller.py
+++ src/controller.py
@@ -118,6 +118,6 @@ class Controller:
             self.obstacle.obstacle_group.empty()
       font = pygame.font.Font(None, 48)
       msg = font.render("Game Over! Click space to try again!", False, "black")
-      self.screen.blit(msg, (575, 400))
+      self.screen.blit(msg, (450, 400))
       pygame.display.flip()
 
