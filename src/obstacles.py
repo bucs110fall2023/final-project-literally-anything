7dd8f6ef253259c47d73db681fd5738e2486ba76
stevenlin22 <slin22@binghamton.edu>
2023-12-08 12:44:29 -0500

    Done

--- src/obstacles.py
+++ src/obstacles.py
@@ -19,7 +19,7 @@ class Obstacles(pygame.sprite.Sprite):
             self.obstacle_group.add(Asteroid(self.x, self.y))
         elif obstacle_type == 1:
             self.obstacle_group.add(Radar(self.x, self.y))
-        print(f'"number of sprites"{len(self.obstacle_group.sprites())}')
+        # print(f'"number of sprites"{len(self.obstacle_group.sprites())}')
 
     def draw(self, screen):
         for i in self.obstacle_group:
@@ -28,12 +28,12 @@ class Obstacles(pygame.sprite.Sprite):
 class Asteroid(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        asteroid_heights = [650, 700, 750]
-        self.image = pygame.transform.scale(pygame.image.load("assets/Asteroid.png"), (150,150))
+        asteroid_heights = [500, 685, 690, 700, 750]
+        self.image = pygame.transform.scale(pygame.image.load("assets/Asteroid.png"), (130,130))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.rect.centery = asteroid_heights[random.randrange(0,3)]
+        self.rect.centery = asteroid_heights[random.randrange(0,5)]
     def draw(self, screen):
         if self.image is not None:
             screen.blit(self.image, (self.rect.x, self.rect.y))
@@ -43,11 +43,14 @@ class Asteroid(pygame.sprite.Sprite):
         if self.rect.x <= -100:
             self.kill()
         # print("updating Asteroid")
+    def get_rect(self):
+        self.rect = (self.rect.x, self.rect.y)
+        return self.rect
 
 class Radar(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load("assets/Radar.png"), (200,200))
+        self.image = pygame.transform.scale(pygame.image.load("assets/Radar.png"), (150,150))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -60,3 +63,6 @@ class Radar(pygame.sprite.Sprite):
         if self.rect.x <= -100:
             self.kill()
         # print("updating radar")
+    def get_rect(self):
+        self.rect = (self.rect.x, self.rect.y)
+        return self.rect
