f588025e2e7434d55f5424c28a679bd1925a46b0
Andrew Zou <azou2@binghamton.edu>
2023-12-08 22:05:50 -0500

    docstrings for player and background classes

--- src/background.py
+++ src/background.py
@@ -1,6 +1,9 @@
 import pygame
 
 class Background:
+    """
+    initializes background values,scales image and blits onto screen
+    """
     def __init__(self,screen, width, height, x):
         self.x = x
         self.y = 0
@@ -13,11 +16,20 @@ class Background:
         screen.blit(self.image, (0,0))
         
     def update(self):
+        """
+        Moves the background to the left to create effect of a moving screen // resets x value after it goes offscreen 
+        """
         self.x -= self.background_speed
         if self.x <= -self.width - self.bg_stretch:
             self.x = 0 
         
     def draw(self, screen):
+        """
+        Draws two background images onto screen to create a continous loop
+
+        Args:
+            screen (Surface): surface for background to be blitted onto
+        """
         for i in range(1):
             screen.blit(self.image, (self.x, self.y))
             screen.blit(self.image, ((self.x + self.width + self.bg_stretch, self.y)))
